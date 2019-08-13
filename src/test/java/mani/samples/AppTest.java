04764edb2e384205630befa4ac075ddcbfde0d2a
lokendramani <lokendramani@gmail.com>
2019-08-13 19:25:52 +0530

    Initial Project Structure

---
+++ src/test/java/mani/samples/AppTest.java
@@ -0,0 +1,20 @@
+package mani.samples;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
