14084b1cc22686a3509b9d50835fa22782a2b263
lokendramani <lokendramani@gmail.com>
2021-12-12 11:31:27 +0530

    ReverseArray non recursive and recursive way.

---
+++ src/main/java/mani/samples/arrays/ReverseArray.java
@@ -0,0 +1,55 @@
+package mani.samples.arrays;
+
+public class ReverseArray {
+    int []arr;
+
+    public ReverseArray(int []arr){
+        this.arr = arr;
+    }
+
+    void reverse(){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start < end ){
+            swapPos(start, end);
+            start++;
+            end--;
+
+        }
+    }
+
+    void reverseRecursively(int start, int end){
+        if (start > end ){
+            return ;
+
+        }
+        swapPos(start, end);
+        start++;
+        end--;
+        reverseRecursively(start, end);
+    }
+
+    void printArray(){
+        for (int a = 0; a < arr.length; a++) {
+            System.out.println(arr[a]);
+        }
+        System.out.println();
+    }
+
+    private void swapPos(int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+
+    }
+
+    public static void main(String[] args) {
+        int []array= {1,2,3,4,5};
+        ReverseArray rv=new ReverseArray(array);
+        rv.reverse();
+        rv.printArray();
+        rv.reverseRecursively(0,4);
+        rv.printArray();
+    }
+
+}
